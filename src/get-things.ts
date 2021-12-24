9d9f3ba4dbdcf594b528a15ba7c43befbe5e7314
Nick Christensen <nchristensen@indeed.com>
2021-12-24 03:08:25 -0600

    Spawn sqlite process to fetch things db as json

--- src/get-things.ts
+++ src/get-things.ts
@@ -1,56 +1,37 @@
-import { promises as fs } from "fs";
 import { homedir } from "os";
 import * as path from "path";
-import initSqlJs from "sql.js";
+import spawn from "spawn-promise";
 
-const getThings = async () => {
+const getThings = async (): Promise<any> => {
 	const dbPath = path.join(
 		homedir(),
 		"Library",
 		"Group Containers",
 		"JLMPQHK86H.com.culturedcode.ThingsMac",
-		"Things.sqlite3"
+		"Things Database.thingsdatabase",
+		"main.sqlite"
 	);
 
-	console.time("db");
+	const query = `SELECT
+		task.uuid, task.type, task.title, project.title as project, area.title as area
+		FROM TMTask task
+		LEFT OUTER JOIN TMTask project ON TASK.project = project.uuid
+		LEFT OUTER JOIN TMArea area ON TASK.area = area.uuid
+		LEFT OUTER JOIN TMTask heading ON TASK.actionGroup = heading.uuid
+		WHERE task.trashed = 0 AND task.status = 0
+		ORDER BY task.creationDate`;
 
-	const dbFile = await fs.readFile(dbPath);
-	// Breaks with error:
-	// sql-wasm.js:166 Fetch API cannot load app://obsidian.md/Applications/Obsidian.app/Contents/Resources/electron.asar/renderer/sql-wasm.wasm. URL scheme "app" is not supported.
-	const SQL = initSqlJs({});
-	const db = new SQL.Database(dbFile);
-
-	console.timeEnd("db");
-
-	// let db = await open({
-	// 	filename: dbPath,
-	// 	driver: Database,
-	// 	mode: OPEN_READONLY,
-	// });
-
-	// let things = await db.all(`
-	// 	SELECT
-	// 		CASE
-	// 			WHEN AREA.title IS NOT NULL THEN AREA.title
-	// 			WHEN PROJECT.title IS NOT NULL THEN PROJECT.title
-	// 			WHEN HEADING.title IS NOT NULL THEN HEADING.title
-	// 			ELSE "(No Context)"
-	// 		END,
-	// 	TASK.title
-	// 	FROM TMTask TASK
-	// 	LEFT OUTER JOIN TMTask PROJECT ON TASK.project = PROJECT.uuid
-	// 	LEFT OUTER JOIN TMArea AREA ON TASK.area = AREA.uuid
-	// 	LEFT OUTER JOIN TMTask HEADING ON TASK.actionGroup = HEADING.uuid
-	// 	WHERE TASK.trashed = 0 AND TASK.status = 0 AND TASK.type = 0
-	// 	ORDER BY TASK.creationDate
-	// 	LIMIT all
-	// `);
-
-	return [
-		{ label: "Google", url: "https://google.com" },
-		{ label: "Apple", url: "https://apple.com" },
-		{ label: "Microsoft", url: "https://microsoft.com" },
-	];
+	try {
+		const buffer = await spawn("sqlite3", [
+			dbPath,
+			"-json",
+			"-readonly",
+			query,
+		]);
+		return JSON.parse(buffer.toString());
+	} catch (e) {
+		console.error(e);
+	}
 };
 
 export default getThings;
