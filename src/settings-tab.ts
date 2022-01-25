1a35fbc0685040018d0ddde959c2b198da1f3c6f
Nick Christensen <nchristensen@indeed.com>
2022-01-24 22:45:08 -0600

    Add comments for bread crumbs

--- src/settings-tab.ts
+++ src/settings-tab.ts
@@ -43,6 +43,9 @@ export default class ThingsSettingTab extends PluginSettingTab {
 		/*
 		 * This isn't working. Obsidian (electron?) seems to be making a copy of the db file instead of returning the original
 		 * The path returned is /var/folders/0l/q28v0x1s2152jkqz7s4gbzp80000gp/T/md.obsidian/zip_cache/..., which really doesn't help.
+		 * References:
+		 * - [obsidian-fantasy-calendar/settings.ts](https://github.com/valentine195/obsidian-fantasy-calendar/blob/master/src/settings/settings.ts#L106-L144)
+		 * - [electron/file-object.md](https://github.com/electron/electron/blob/main/docs/api/file-object.md)
 		 */
 
 		// let dbSetting = new Setting(containerEl).setName("Things database");
